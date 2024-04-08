deaef9d3bfe27b47b60d6828e2b698f67ff188b3
Caio H Perlin <caiohperlin@gmail.com>
2024-04-08 18:15:18 -0400

    build: boilerplate express + dotenv

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.DEV_PORT;
+
+app.get('/', (req, res) => {
+    res.status(200).send("the API is running nicely")
+})
+
+app.listen(
+    PORT,
+    () => console.log(`Listening on ${PORT}`)
+);
