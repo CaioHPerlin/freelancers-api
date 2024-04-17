77abdcfd043eb76a5d62204d7ef31cce4e8e22a4
Caio H Perlin <caiohperlin@gmail.com>
2024-04-17 17:35:23 -0400

    fix: allow all origins on CORS

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const PORT = process.env.DEV_PORT;
 
 const cors = require('cors');
 
-const corsOptions = {
-    origin: ["http://localhost:5500", `http://localhost:${PORT}`],
-    methods: "GET,PUT,POST,DELETE",
-    allowedHeaders:"Content-Type, Authorization",
-    credentials: true
-};
+// const corsOptions = {
+//     origin: ["http://localhost:5500", `http://localhost:${PORT}`],
+//     methods: "GET,PUT,POST,DELETE",
+//     allowedHeaders:"Content-Type, Authorization",
+//     credentials: true
+// };
 
 //Middleware
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 //Freelancers
 app.get('/freelancers', freelancerController.getAll);
