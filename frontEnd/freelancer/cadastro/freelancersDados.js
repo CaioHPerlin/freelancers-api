4070a89519d4e308eeee6d0aa8bed80a92ae2737
willianifms <williancardoso00004@gmail.com>
2024-04-18 13:15:43 -0400

    feat: select cidades por estados

--- frontEnd/freelancer/cadastro/freelancersDados.js
+++ frontEnd/freelancer/cadastro/freelancersDados.js
@@ -1,30 +1,90 @@
-const dadosfreelancers = document.getElementById('dadosFreelancers');
+const dadosfreelancers = document.getElementById("dadosFreelancers");
 
-dadosfreelancers.addEventListener('submit', evento => {
+dadosfreelancers.addEventListener("submit", (evento) => {
   evento.preventDefault();
   const freelancersData = new FormData(dadosfreelancers);
   const data = Object.fromEntries(freelancersData);
 
-  fetch('https://sebrae-api.onrender.com/freelancers', {
-    method: 'POST',
+  fetch("https://sebrae-api.onrender.com/freelancers", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
     .then((res) => res.json())
-    .then(data => {
+    .then((data) => {
       console.log(data);
-      alert('Cadastro realizado com sucesso!');
+      window.location.href = '../sucessoCadastro/sucesso.html';
     })
-    .catch((err) => {   
+    .catch((err) => {
       console.error(err);
-      alert('Erro ao cadastrar!');
+      alert("Erro ao cadastrar!");
     });
 });
 
-
 //(data) => {
-    //console.log('Successo:', data);
-    //alert('Cadastro realizado com sucesso!');
-   // window.location.href = 'freelancers.html';
+//console.log('Successo:', data);
+//alert('Cadastro realizado com sucesso!');
+// window.location.href = 'freelancers.html';
+
+//select api de estados e cidades
+const uf = document.querySelector('#state')
+const local = document.querySelector('#city')
+
+
+function selectUF() {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            json.sort(function organizar(x, y) {
+                let a = x.sigla.toUpperCase(),
+                    b = y.sigla.toUpperCase();
+                return a == b ? 0 : a > b ? 1 : -1;
+            });
+            for (let i = 0; i < json.length; i++) {
+                let newopt = document.createElement('option');
+                newopt.setAttribute('value', json[i].id);
+                newopt.setAttribute('id', json[i].sigla);
+                newopt.innerText = json[i].sigla;
+                uf.appendChild(newopt);
+            }
+        })
+}
+
+uf.addEventListener('change', (element) => {
+    local.innerHTML = '';
+    local.removeAttribute('disabled');
+    selectLocal(element.target.value);
+});
+
+function selectLocal(selectedUf) {
+    local.innerHTML = '<option value="" disabled selected>Selecione</option>';
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            json.sort(function organizar(x, y) {
+                let a = x.nome.toUpperCase(),
+                    b = y.nome.toUpperCase();
+                return a == b ? 0 : a > b ? 1 : -1;
+            });
+            for (let i = 0; i < json.length; i++) {
+                let newopt = document.createElement('option');
+                newopt.setAttribute('value', json[i].nome);
+                newopt.innerText = json[i].nome;
+                local.appendChild(newopt);
+            }
+        })
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    uf.innerHTML = '<option id="optVoid" value="" disabled selected>Selecione</option>';
+    selectUF();
+});
+
+
