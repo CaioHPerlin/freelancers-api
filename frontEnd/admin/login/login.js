a601dcf12fe9e089da3c2fc305ad22ea27ab745d
willianifms <williancardoso00004@gmail.com>
2024-04-22 12:22:26 -0400

    feat: loader de carregamento

--- frontEnd/admin/login/login.js
+++ frontEnd/admin/login/login.js
@@ -2,6 +2,8 @@ const formAuth = document.getElementById('auth');
 
 formAuth.addEventListener('submit', (e) => {
 	e.preventDefault();
+	const loaderContainer = document.getElementById('loadercont')
+	loaderContainer.innerHTML = '<h1 class="loader"></h1>';
 
 	const credentials = {
 		username: formAuth.username.value,
@@ -24,13 +26,14 @@ formAuth.addEventListener('submit', (e) => {
 			return res.json();
 		})
 		.then((data) => {
+			loaderContainer.innerHTML = '';
 			console.log(data);
-            
             localStorage.setItem('token', data.token);
 
 			window.location.href = '../dadosFreelancers/admin.html';
 		})
 		.catch((err) => {
+			loaderContainer.innerHTML = '';
 			console.error(err);
 			alert(err.message);
 		});
