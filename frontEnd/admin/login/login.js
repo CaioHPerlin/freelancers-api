bf737d974f5a66e7ea47bf24ac5b2f7cc275b318
Caio H Perlin <caiohperlin@gmail.com>
2024-04-22 13:01:59 -0400

    Revert "feat: loader de carregamento"
    This reverts commit a601dcf12fe9e089da3c2fc305ad22ea27ab745d.

--- frontEnd/admin/login/login.js
+++ frontEnd/admin/login/login.js
@@ -2,8 +2,6 @@ const formAuth = document.getElementById('auth');
 
 formAuth.addEventListener('submit', (e) => {
 	e.preventDefault();
-	const loaderContainer = document.getElementById('loadercont')
-	loaderContainer.innerHTML = '<h1 class="loader"></h1>';
 
 	const credentials = {
 		username: formAuth.username.value,
@@ -26,14 +24,13 @@ formAuth.addEventListener('submit', (e) => {
 			return res.json();
 		})
 		.then((data) => {
-			loaderContainer.innerHTML = '';
 			console.log(data);
+            
             localStorage.setItem('token', data.token);
 
 			window.location.href = '../dadosFreelancers/admin.html';
 		})
 		.catch((err) => {
-			loaderContainer.innerHTML = '';
 			console.error(err);
 			alert(err.message);
 		});
