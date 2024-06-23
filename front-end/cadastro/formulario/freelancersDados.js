fd23a51d6e033a8952e0431b2a359a13f290cdec
willian <williancardoso00004@gmail.com>
2024-06-23 14:37:19 -0400

    fix: submit image retate user

--- front-end/cadastro/formulario/freelancersDados.js
+++ front-end/cadastro/formulario/freelancersDados.js
@@ -1,16 +1,146 @@
-document.getElementById('completo').addEventListener('change', function () {
-	if (this.checked) {
-		document.getElementById('incompleto').checked = false;
-	}
+const completoCheckbox = document.getElementById('completo');
+const incompletoCheckbox = document.getElementById('incompleto');
+
+completoCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+        incompletoCheckbox.checked = false;
+    }
 });
 
-document.getElementById('incompleto').addEventListener('change', function () {
-	if (this.checked) {
-		document.getElementById('completo').checked = false;
-	}
+incompletoCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+        completoCheckbox.checked = false;
+    }
 });
 
 const dadosfreelancers = document.getElementById('dadosFreelancers');
+const profilePicture = document.getElementById('profilePicture');
+const facialPicture = document.getElementById('facialPicture');
+
+dadosfreelancers.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    if (!completoCheckbox.checked && !incompletoCheckbox.checked) {
+        alert('Por favor, selecione se o curso está completo ou incompleto.');
+        return;
+    }
+
+    const freelancersData = new FormData(dadosfreelancers);
+    const loaderContainer = document.getElementById('loadercont');
+    loaderContainer.innerHTML = '<h1 class="loader"></h1>';
+
+    let profileBlob;
+    let facialBlob;
+
+    const getBlobFromCanvas = (image, degree) => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        const img = new Image();
+        img.src = image.src;
+        return new Promise((resolve) => {
+            img.onload = () => {
+                const width = img.width;
+                const height = img.height;
+                
+                if (degree % 180 !== 0) {
+                    canvas.width = height;
+                    canvas.height = width;
+                } else {
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+
+                ctx.translate(canvas.width / 2, canvas.height / 2);
+                ctx.rotate((degree * Math.PI) / 180);
+                ctx.drawImage(img, -width / 2, -height / 2);
+
+                canvas.toBlob(blob => resolve(blob), 'image/jpeg');
+            };
+        });
+    };
+
+    const requests = [
+        getBlobFromCanvas(profilePreview, profileRotation).then(blob => profileBlob = blob),
+        getBlobFromCanvas(facialPreview, facialRotation).then(blob => facialBlob = blob),
+    ];
+
+    Promise.all(requests).then(() => {
+        if (profileBlob) freelancersData.set('profilePicture', profileBlob, 'profile.jpg');
+        if (facialBlob) freelancersData.set('facialPicture', facialBlob, 'facial.jpg');
+
+        dadosfreelancers.course.value += completoCheckbox.checked ? ' (Completo)' : ' (Incompleto)';
+
+        fetch('https://api.nkarbits.com.br/freelancers', {
+            method: 'POST',
+            body: freelancersData,
+        })
+        .then((res) => {
+            if (!res.ok) {
+                return res.json().then((body) => {
+                    throw new Error(body.message);
+                });
+            }
+            return res.json();
+        })
+        .then((data) => {
+            loaderContainer.innerHTML = '';
+            console.log(data);
+            window.location.href = '../sucesso/';
+        })
+        .catch((err) => {
+            loaderContainer.innerHTML = '';
+            console.error(err);
+            alert(err);
+        });
+    });
+});
+
+// select api de estados e cidades
+const uf = document.querySelector('#state');
+const local = document.querySelector('#city');
+
+function selectUF() {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then(response => response.json())
+        .then(json => {
+            json.sort((x, y) => x.sigla.localeCompare(y.sigla));
+            json.forEach(estado => {
+                const newopt = document.createElement('option');
+                newopt.setAttribute('value', estado.sigla);
+                newopt.setAttribute('id', estado.sigla);
+                newopt.innerText = estado.sigla;
+                uf.appendChild(newopt);
+            });
+        });
+}
+
+uf.addEventListener('change', event => {
+    local.innerHTML = '';
+    local.removeAttribute('disabled');
+    selectLocal(event.target.value);
+});
+
+function selectLocal(selectedUf) {
+    local.innerHTML = '<option value="" disabled selected>Selecione</option>';
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response => response.json())
+        .then(json => {
+            json.sort((x, y) => x.nome.localeCompare(y.nome));
+            json.forEach(cidade => {
+                const newopt = document.createElement('option');
+                newopt.setAttribute('value', cidade.nome);
+                newopt.innerText = cidade.nome;
+                local.appendChild(newopt);
+            });
+        });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    uf.innerHTML = '<option id="optVoid" value="" disabled selected>Selecione</option>';
+    selectUF();
+});
+
 const rotateProfileButton = document.getElementById('rotateProfileButton');
 const rotateFacialButton = document.getElementById('rotateFacialButton');
 const profilePreview = document.getElementById('profilePreview');
@@ -20,93 +150,33 @@ let profileRotation = 0;
 let facialRotation = 0;
 
 rotateProfileButton.addEventListener('click', function() {
-	profileRotation = (profileRotation + 90) % 360;
-	profilePreview.style.transform = `rotate(${profileRotation}deg)`;
+    profileRotation += 90;
+    profilePreview.style.transform = `rotate(${profileRotation}deg)`;
 });
 
 rotateFacialButton.addEventListener('click', function() {
-	facialRotation = (facialRotation + 90) % 360;
-	facialPreview.style.transform = `rotate(${facialRotation}deg)`;
+    facialRotation += 90;
+    facialPreview.style.transform = `rotate(${facialRotation}deg)`;
 });
 
-document.getElementById('profilePicture').addEventListener('change', function() {
-	const file = this.files[0];
-	const reader = new FileReader();
+profilePicture.addEventListener('change', function() {
+    const file = this.files[0];
+    const reader = new FileReader();
 
-	reader.onload = function(e) {
-		profilePreview.style.display = 'block';
-		profilePreview.src = e.target.result;
-	};
+    reader.onload = function(e) {
+        profilePreview.src = e.target.result;
+    };
 
-	reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
 });
 
-document.getElementById('facialPicture').addEventListener('change', function() {
-	const file = this.files[0];
-	const reader = new FileReader();
+facialPicture.addEventListener('change', function() {
+    const file = this.files[0];
+    const reader = new FileReader();
 
-	reader.onload = function(e) {
-		facialPreview.style.display = 'block';
-		facialPreview.src = e.target.result;
-	};
+    reader.onload = function(e) {
+        facialPreview.src = e.target.result;
+    };
 
-	reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
 });
-
-dadosfreelancers.addEventListener('submit', (evento) => {
-	evento.preventDefault();
-
-	if (!document.getElementById('completo').checked && !document.getElementById('incompleto').checked) {
-		alert('Por favor, selecione se o curso está completo ou incompleto.');
-		return;
-	}
-
-	const formData = new FormData(dadosfreelancers);
-	formData.append('profileRotation', profileRotation);
-	formData.append('facialRotation', facialRotation);
-
-	const profileBlob = dataURLtoBlob(profilePreview.src);
-	const facialBlob = dataURLtoBlob(facialPreview.src);
-
-	formData.append('profilePicture', profileBlob, 'profilePicture.jpg');
-	formData.append('facialPicture', facialBlob, 'facialPicture.jpg');
-
-	const loaderContainer = document.getElementById('loadercont');
-	loaderContainer.innerHTML = '<h1 class="loader"></h1>';
-
-	fetch('https://api.nkarbits.com.br/freelancers', {
-		method: 'POST',
-		body: formData,
-	})
-		.then((res) => {
-			if (!res.ok) {
-				return res.json().then((body) => {
-					throw new Error(body.message);
-				});
-			}
-
-			return res.json();
-		})
-		.then((data) => {
-			loaderContainer.innerHTML = '';
-			console.log(data);
-			window.location.href = '../sucesso/';
-		})
-		.catch((err) => {
-			loaderContainer.innerHTML = '';
-			console.error(err);
-			alert(err);
-		});
-});
-
-function dataURLtoBlob(dataurl) {
-	const arr = dataurl.split(',');
-	const mime = arr[0].match(/:(.*?);/)[1];
-	const bstr = atob(arr[1]);
-	let n = bstr.length;
-	const u8arr = new Uint8Array(n);
-	while (n--) {
-		u8arr[n] = bstr.charCodeAt(n);
-	}
-	return new Blob([u8arr], { type: mime });
-}
